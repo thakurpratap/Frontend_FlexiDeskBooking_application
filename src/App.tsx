660ffa791903820894b1c77f382eb1da9580b0d2
pratap singh <pratapsingh@prataps-MacBook-Air.local>
2024-11-25 18:49:46 +0530

    API integration and filter Ui design completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ import Sidebar from "./Components/Sidebar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Inventory from "./Inventort";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HomeIcon } from "./assets/icons/Desk";
+// import Home from "./pages/Home";
+import { DataProvider } from "./pages/DataContext";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
+      <QueryClientProvider client={queryClient}>
       <BrowserRouter >
+      <DataProvider> 
         <div className="flex h-screen">
           <Sidebar />
           <div className="flex-1 flex flex-col">
@@ -21,7 +29,9 @@ function App() {
         </div>
         </div>
         </div>
+        </DataProvider> 
       </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/Inventort.tsx
+++ src/Inventort.tsx
@@ -1,25 +1,44 @@
 import React, { useState } from "react";
-import {Box,Button,ButtonBase,IconButton,InputBase,Menu,MenuItem,Paper,Table,TableBody,TableCell,TableContainer,TableHead,TableRow,Typography,} from "@mui/material";
-import FilterListIcon from "@mui/icons-material/FilterList";
+import {Box,Button,ButtonBase,Checkbox,Dialog,DialogActions,DialogContent,DialogTitle,FormControl,FormControlLabel,IconButton,InputAdornment,InputBase,Menu,MenuItem,Paper,Select,Table,TableBody,TableCell,TableContainer,TableHead,TableRow,TextField,Typography,} from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { DeleteIcon, EditeIcon, FilterIcon, ResentIcon, SearchIcon } from "./assets/icons/Desk";
-import { Margin } from "@mui/icons-material";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday"
+import { useDataContext } from "./pages/DataContext";
 
 const Inventory = () => {
-  const rows = [
-    {id: 10001,name: "Dianne Russell",email: "dianne.r@mail.com",type: "Hot Desk",invitees: "Steve Doe +2", date: "18-07-2023",status: "PENDING",},
-    {id: 10002,name: "Jerome Bell", email: "jerome@mail.com",type: "Hot Desk",invitees: "Michael Scott +2",date: "19-07-2023", status: "PAID",  },
-    { id: 10003, name: "Wade Warren", email: "w.wade@mail.com", type: "Meeting Room", invitees: "Jim Halpert +5", date: "12-07-2023", status: "PAID" }, 
-    { id: 10004, name: "Ralph Edwards", email: "ralph@mail.com", type: "Hot Desk", invitees: "Steve Doe +2", date: "10-07-2023", status: "PAID" }, 
-    { id: 10005, name: "Cody Fisher", email: "codyfisher@mail.com", type: "Meeting Room", invitees: "Jim Halpert +3", date: "08-07-2023", status: "PAID" }, 
-    { id: 10006, name: "Eleanor Pena", email: "e.pena@mail.com", type: "Meeting Room", invitees: "Dwight Shrute +2", date: "18-06-2023", status: "PAID" }
-  ];
+
+  const { bookings, isLoading , searchBookings, searchResults, isSearching,} = useDataContext();
+ 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [selectedRow, setSelectedRow] = useState<number | null>(null);
+  const [selectedRow, setSelectedRow] = useState<number | string  | null>(null);
+  const [filterModalOpen, setFilterModalOpen] = useState(false);
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (searchQuery.trim()) {
+      await searchBookings(searchQuery);
+    }
+  };
+
+  const displayedBookings = searchResults && searchResults.length > 0 ? searchResults : bookings;
+
+  const toggleFilterModal = () => {
+    setFilterModalOpen(!filterModalOpen);
+  };
+
+  const handleApplyFilters = () => {
+    setFilterModalOpen(false); 
+  };
+
+  const handleClearFilters = () => {
+    setFilterModalOpen(false);
+  };
 
   const handleMenuOpen = (
     event: React.MouseEvent<HTMLElement>,
-    rowId: number
+    rowId: string
   ) => {
     setAnchorEl(event.currentTarget);
     setSelectedRow(rowId);
@@ -30,6 +49,9 @@ const Inventory = () => {
     setSelectedRow(null);
   };
 
+  if (isLoading || isSearching) {
+    return <Typography>Loading...</Typography>;
+  }
   return (
     <>
       <div
@@ -47,15 +69,19 @@ const Inventory = () => {
       <Box sx={{ padding: 2 , marginLeft:"2px"}}>
         <Box
           sx={{display: "flex",justifyContent: "",alignItems: "center",marginBottom: 2, }}>
-          <Paper     
+          <Paper    
+          onSubmit={handleSearch} 
             component="form"
             sx={{display: "flex",alignItems: "center",width: "640px",height: "40px",padding: "0 8px",border: "1px solid #BDBDBD",boxShadow: "none",borderRadius: "4px",
               marginBottom: "16px",background:"#F7F7F7", marginLeft:"13px" }}>
             <SearchIcon/>
             <InputBase
-              style={{ flex: 1, marginLeft:"6px" }} placeholder="Search" inputProps={{ "aria-label": "search" }}/>
+              style={{ flex: 1, marginLeft:"6px" }} placeholder="Search" inputProps={{ "aria-label": "search" }}
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              />
           </Paper>
-            <ButtonBase sx={{marginLeft: "9px", marginBottom:"12px",}}>   
+            <ButtonBase sx={{marginLeft: "9px", marginBottom:"12px",}} onClick={toggleFilterModal}>   
                <FilterIcon/> 
                </ButtonBase>
           <Button
@@ -97,26 +123,34 @@ const Inventory = () => {
                 <TableCell>
                   <Typography sx={{ fontSize: "12px",fontWeight: 400,lineHeight: "16px",color: "#717171",}}>BOOKING DATE</Typography>
                 </TableCell>
+                {/* <TableCell>
+                  <Typography sx={{ fontSize: "12px",fontWeight: 400,lineHeight: "16px",color: "#717171",}}>BOOKING STATUS</Typography>
+                </TableCell> */}
                 <TableCell>
                   <Typography sx={{ fontSize: "12px",fontWeight: 400,lineHeight: "16px",color: "#717171",}}>STATUS</Typography>
                 </TableCell>
               {/* </TableRow> */}
             {/* </TableHead> */}
             <TableBody>
-              {rows.length > 0 ? (
-              rows.map((row) => (
+              {bookings.length > 0 ? (
+              bookings.map((row) => (
                 <TableRow key={row.id}>
-                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}} >{row.id}</TableCell>
-                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.name}</TableCell>
-                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.email}</TableCell>
-                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.type}</TableCell>
-                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.invitees}</TableCell>
-                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.date}</TableCell>
+                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}} >{row._id.slice(0, 5)}</TableCell>
+                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.guest_name}</TableCell>
+                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.guest_email}</TableCell>
+                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.booking_type}</TableCell>
+                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>
+                  {row.invitee.length > 0 ? `${row.invitee[0].invitee_name}${row.invitee.length > 1 ? ` +${row.invitee.length - 1}` : ""}` : "No Invitees"}
+                  </TableCell>
+                  <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.createdAt.substring(0, 10)}</TableCell>
+                  {/* <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>{row.company_name}</TableCell> */}
+
                   <TableCell style={{fontSize: "14px",fontWeight: 400,lineHeight: "20.3px",color: "#222222",}}>
                     <Typography
-                      sx={{display: "inline-block",backgroundColor:row.status === "PENDING" ? "#FFBDAD" : "#79F2C0",color: "#42526E",fontWeight: "bold",padding: "4px 8px",
+                      sx={{display: "inline-block" ,backgroundColor:row.guest_checkin_status ? "#FFBDAD" : "#79F2C0", color: "#42526E",fontWeight: "bold",padding: "4px 8px",
                         borderRadius: "4px",textAlign: "center", width: "auto" }} >
-                      {row.status}
+                      {/* {row.guest_checkin_status} */}
+                      {row.payment_status ? "PAID" : "Pending"}
                     </Typography>
                   </TableCell>
 
@@ -124,15 +158,25 @@ const Inventory = () => {
                     <IconButton
                       aria-label="more"
                       onClick={(e) => handleMenuOpen(e, row.id)}
-                    >
+                      sx={{
+                        transform: 'translateX(-5%)',
+                        backgroundColor: 'transparent',
+                        boxShadow: 'none', 
+                        border: 'none', 
+                      }}
+                      >
                       <MoreVertIcon />
                     </IconButton>
-
                     <Menu
                       anchorEl={anchorEl}
                       open={Boolean(anchorEl) && selectedRow === row.id}
                       onClose={handleMenuClose}  
-                      sx={{transform: 'translateX(-5%)',}}>
+                      sx={{transform: 'translateX(-5%)',
+                        '& .MuiPaper-root': {
+                          boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.05)',
+                          border: '1px solid rgba(0, 0, 0, 0.12)', 
+                        },
+                      }}>
                      <MenuItem onClick={handleMenuClose} sx={{width:"224px", height:"32px"}}>
                       <span style={{ marginRight: "14px" }}><ResentIcon /></span>
                       <span style={{fontSize: '14px',  padding: '2px 4px', color: '#172B4D'}}> Resend Payment Email</span>         
@@ -146,8 +190,8 @@ const Inventory = () => {
                         <span style={{fontSize: '14px',  padding: '2px 4px', color: '#172B4D'}}> Cancel Booking</span>                   
                       </MenuItem>
                     </Menu>
-
                   </TableCell>
+                  
                </TableRow>
                   ))
                 ) : (
@@ -157,11 +201,84 @@ const Inventory = () => {
                     </TableCell>
                   </TableRow>
                 )}
-              {/* ))} */}
+
             </TableBody>
           </Table>
         </TableContainer>
       </Box>
+
+
+      {/* Filter Modal */}
+      <Dialog open={filterModalOpen} onClose={toggleFilterModal} sx={{
+    '& .MuiDialog-paper': {
+      width: '426px',
+      height: '404px',
+    },
+  }}>
+        <DialogTitle sx={{ fontWeight: "bold", fontSize: "18px" }}>
+          Filter Options
+        </DialogTitle>
+        <DialogContent>
+          <FormControl fullWidth sx={{ marginBottom: "16px" }}>
+            <Typography variant="subtitle1">Booking Type</Typography>
+            <Select
+              // value={bookingType}
+              // onChange={(e) => setBookingType(e.target.value)}
+              displayEmpty
+              sx={{ marginTop: "8px" }}
+            >
+              <MenuItem value="All">All</MenuItem>
+              <MenuItem value="Hot Desk">Hot Desk</MenuItem>
+              <MenuItem value="Meeting Room">Meeting Room</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl fullWidth sx={{ marginBottom: "16px" }}>
+          <Typography className="text-sm font-medium">Visit Date</Typography>
+          <TextField
+            // value={visitDate}
+            // onChange={(e) => setVisitDate(e.target.value)}
+            className="mt-1 border border-gray-300 rounded-lg"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton>
+                    <CalendarTodayIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </FormControl>
+        <FormControl fullWidth sx={{ marginBottom: "16px" }}>
+          <Typography className="text-sm font-medium">Booking Date</Typography>
+          <TextField
+            // value={bookingDate}
+            // onChange={(e) => setBookingDate(e.target.value)}
+            className="mt-1 border border-gray-300 rounded-lg"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton>
+                    <CalendarTodayIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </FormControl>
+        </DialogContent>
+
+       <DialogActions className="space-x-4">
+        <Button
+          onClick={handleClearFilters} className="bg-[#F7F7F7] rounded-md px-4 py-2">  
+          Cancel
+        </Button>
+        <Button
+          onClick={handleApplyFilters} className="bg-[#343434] rounded-md px-4 py-2"  >
+          Apply
+        </Button>
+      </DialogActions>
+      </Dialog>
     </>
   );
 };
