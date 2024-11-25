affc9acdc1a89ae13c2d76dc3b029fcf45027363
dinesh <dineshprajapati9867@gmail.com>
2024-11-25 17:30:29 +0530

    resolve Issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,89 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-//import Home from "./pages/Home";
 import Inventory from "./Inventort";
 import PaymentDetails from "./Components/PaymentDetails";
 import NewBooking from "./Components/NewBooking";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
         <div className="flex h-screen">
           <Sidebar />
           <div className="flex-1 flex flex-col">
             <Navbar />
-
-            <div className="flex-1 overflow-auto ">
+            <div className="flex-1 overflow-auto">
               <Routes>
-                {/* <Route path="/" element={<Home />} /> */}
+                {/* Define all your routes here */}
                 <Route path="/" element={<Inventory />} />
                 <Route path="/payment-details" element={<PaymentDetails />} />
                 <Route path="/new-booking" element={<NewBooking />} />
               </Routes>
             </div>
           </div>
-            <Navbar /> 
-        <div className="flex-1 overflow-auto ">
-        <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/inventory" element={<Inventory />} />
-        </Routes>
         </div>
-        </div>
-        </div>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import "./App.css";
+// import Sidebar from "./Components/Sidebar";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Navbar from "./Components/Navbar";
+// //import Home from "./pages/Home";
+// import Inventory from "./Inventort";
+// import PaymentDetails from "./Components/PaymentDetails";
+// import NewBooking from "./Components/NewBooking";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <div className="flex h-screen">
+//           <Sidebar />
+//           <div className="flex-1 flex flex-col">
+//             <Navbar />
+
+//             <div className="flex-1 overflow-auto ">
+//               <Routes>
+//                 {/* <Route path="/" element={<Home />} /> */}
+//                 <Route path="/" element={<Inventory />} />
+//                 <Route path="/payment-details" element={<PaymentDetails />} />
+//                 <Route path="/new-booking" element={<NewBooking />} />
+//               </Routes>
+//             </div>
+//           </div>
+//             <Navbar /> 
+//         <div className="flex-1 overflow-auto ">
+//         <Routes>
+//         {/* <Route path="/" element={<Home />} /> */}
+//         <Route path="/inventory" element={<Inventory />} />
+//         </Routes>
+//         </div>
+//         </div>
+//         </div>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
